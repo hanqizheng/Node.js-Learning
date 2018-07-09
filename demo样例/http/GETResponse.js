b1b13a2df78b8350c73afd860fa7f2b3e788fcc5
hanqizheng <357571432@qq.com>
2018-07-09 18:47:53 +0800

    dialog 2018-07-09

---
+++ demo样例/http/GETResponse.js
@@ -0,0 +1,12 @@
+var http = require('http');
+
+http.get('http://id.qq.com/', function(res){
+    var data = '';
+    res.setEncoding('utf8');
+    res.on('data', function(chunk){
+        data += chunk;
+    });
+    res.on('end', function(){
+        console.log(data);
+    });
+});
